2e554ea00ddc2227d8def7fbe0b998989f9a4b17
Adams Shittu <shttdms@gmail.com>
2018-10-17 08:01:43 +0100

    set up eslint and airbnb

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,23 @@
 module.exports = {
-    "extends": "airbnb-base"
+    extends: "airbnb-base",
+    rules: {
+        "no-console": 0,
+        "semi":["error", "always"],
+        "camelcase":"off",
+        "comma-dangle":"off",
+        "indent":["error", 2],
+        "prefer-const": ["error", {
+            "destructuring": "any",
+            "ignoreReadBeforeAssign": false
+        }],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ]
+    },
+    env: {
+        "browser": true,
+        "node": true,
+        "mocha":true
+    }
 };
--- SERVER/app.js
+++ SERVER/app.js
@@ -2,6 +2,13 @@ import express from 'express';
 import bodyParser from 'body-parser';
 
 const app = express();
- 
-// parse various different custom JSON types as JSON
-app.use(bodyParser.json({ type: 'application/*+json' }))
+
+app.use(bodyParser.json());
+
+const PORT = 3000 || process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`app is listening on port ${PORT}`);
+});
+
+export default app;
